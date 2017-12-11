6335e1de8b5018d3175c7612c83d1c8b05ad05a2
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-12-11 12:14:18 -0200

    commit

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -5,64 +5,6 @@ import PlaygroundSupport
 
 typealias JSONDictionary = [String: AnyObject]
 
-// ----------------------------------------------------------------------------
-// Place
-// ----------------------------------------------------------------------------
-
-let CREATEDAT: String = "createdAt"
-let ID: String = "id"
-let LOCATION: String = "location"
-let NAME: String = "name"
-let UPDATEDAT: String = "updatedAt"
-
-struct Place {
-    let createdAt: String?
-    let id: String?
-    let location: String?
-    let name: String?
-    let updatedAt: String?
-}
-
-extension Place {
-    init(dictionary: JSONDictionary) {
-        self.createdAt = dictionary[CREATEDAT] as? String
-        self.id = dictionary[ID] as? String
-        self.location = dictionary[LOCATION] as? String
-        self.name = dictionary[NAME] as? String
-        self.updatedAt = dictionary[UPDATEDAT] as? String
-    }
-}
-
-// ----------------------------------------------------------------------------
-// API
-// ----------------------------------------------------------------------------
-
-struct Resource<T> {
-    let url: URL
-    let parse: (Data) -> T?
-}
-
-extension Resource {
-    init(url: URL, parseJSON: @escaping (AnyObject)->(T?)) {
-        self.url = url
-        self.parse = { data in
-            let json = try? JSONSerialization.jsonObject(with: data, options: []) as AnyObject
-            return json.flatMap(parseJSON)
-        }
-    }
-}
-
-final class WebService {
-    
-    func load<T>(resource: Resource<T>, completion: @escaping (T?, Error?) ->()) {
-        URLSession.shared.dataTask(with: resource.url) {(data, _, error) in
-            guard let data = data else { completion(nil, error); return }
-            completion(resource.parse(data), error)
-        }.resume()
-    }
-    
-}
-
 /** Enum is not assigned a default integer value when they are
  * created each element is of type 'Status'.
  */
